1a668d8757619f4148baac86c33cc425e7ffd5a8
Shane Church <shane@s-church.net>
2016-03-25 10:09:35 -0500

    Roman Numerals Kata

---
+++ SogetiAssessment.Console/RomanNumerals.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SogetiAssessment.Console
+{
+    public static class RomanNumerals
+    {
+        private  struct RomanNumeral
+        {
+            public int Number;
+            public string Numeral;
+        }
+
+        private static readonly List<RomanNumeral> numerals = new List<RomanNumeral>()
+        {
+            new RomanNumeral() { Number = 1000, Numeral = "M" },
+            new RomanNumeral() { Number =  900, Numeral = "CM"},
+            new RomanNumeral() { Number =  500, Numeral = "D"},
+            new RomanNumeral() { Number =  400, Numeral = "CD"},
+            new RomanNumeral() { Number =  100, Numeral = "C"},
+            new RomanNumeral() { Number =  90, Numeral = "XC"},
+            new RomanNumeral() { Number =  50, Numeral = "L"},
+            new RomanNumeral() { Number =  40, Numeral = "XL"},
+            new RomanNumeral() { Number =  10, Numeral = "X"},
+            new RomanNumeral() { Number =  9, Numeral = "IX"},
+            new RomanNumeral() { Number =  5, Numeral = "V"},
+            new RomanNumeral() { Number =  4, Numeral = "IV"},
+            new RomanNumeral() { Number =  1, Numeral = "I"},
+        };
+
+        public static string ToRoman(this int arabic)
+        {
+            var workingArabic = arabic;
+            string output = "";
+            foreach(RomanNumeral numeral in numerals)
+            {
+                while(workingArabic >= numeral.Number)
+                {
+                    output += numeral.Numeral;
+                    workingArabic -= numeral.Number;
+                }
+            }
+            return output;
+        }
+
+        public static int ToArabic(this string roman)
+        {
+            int output = 0;
+            foreach(RomanNumeral numeral in numerals)
+            {
+                if(roman.StartsWith(numeral.Numeral))
+                {
+                    output += numeral.Number;
+                    if (roman.Length > numeral.Numeral.Length)
+                    {
+                        output += roman.Substring(numeral.Numeral.Length).ToArabic();
+                    }
+                    return output;
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ SogetiAssessment.Tests/RomanNumeralsTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SogetiAssessment.Console;
+
+namespace SogetiAssessment.Tests
+{
+    [TestClass]
+    public class RomanNumeralsTests
+    {
+        [TestMethod]
+        public void OneToRoman()
+        {
+            int input = 1;
+            string expected = "I";
+            var actual = input.ToRoman();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ThreeToRoman()
+        {
+            int input = 3;
+            string expected = "III";
+            var actual = input.ToRoman();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NineToRoman()
+        {
+            int input = 9;
+            string expected = "IX";
+            var actual = input.ToRoman();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OneThousandSixtySixToRoman()
+        {
+            int input = 1066;
+            string expected = "MLXVI";
+            var actual = input.ToRoman();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OneThousandNineHundredEightyNineToRoman()
+        {
+            int input = 1989;
+            string expected = "MCMLXXXIX";
+            var actual = input.ToRoman();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IToArabic()
+        {
+            int expected = 1;
+            string input = "I";
+            var actual = input.ToArabic();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IIIToArabic()
+        {
+            int expected = 3;
+            string input = "III";
+            var actual = input.ToArabic();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IXToArabic()
+        {
+            int expected = 9;
+            string input = "IX";
+            var actual = input.ToArabic();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MLXVIToArabic()
+        {
+            int expected = 1066;
+            string input = "MLXVI";
+            var actual = input.ToArabic();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MCMLXXXIXToArabic()
+        {
+            int expected = 1989;
+            string input = "MCMLXXXIX";
+            var actual = input.ToArabic();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
