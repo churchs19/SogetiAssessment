f6adf64dd626ba559a1a930bed9e60624130b85b
Shane Church <shane@s-church.net>
2016-03-25 08:55:47 -0500

    Initial Commit - String Calculator

---
+++ SogetiAssessment.Console/StringCalculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SogetiAssessment.Console
+{
+    public class StringCalculator
+    {
+        public int Add (string numbers)
+        {
+            char[] delimiters = new char[] { ',', '\n' };
+            if(numbers.StartsWith("//"))
+            {
+                var delimiter = numbers[numbers.IndexOf('\n') - 1];
+                delimiters = new char[] { delimiter };
+            }
+            int result = 0;
+            if (String.IsNullOrEmpty(numbers)) return result;
+
+            var numberArray = numbers.Split(delimiters);
+            List<string> negatives = new List<string>();
+            foreach(var num in numberArray)
+            {
+                int parsedNum;
+                if(int.TryParse(num, out parsedNum))
+                {
+                    if (parsedNum >= 0)
+                    {
+                        result += parsedNum > 1000 ? 0 : parsedNum;
+                    }
+                    else
+                    {
+                        negatives.Add(num);
+                    }
+                }
+            }
+            if(negatives.Count > 0)
+            {
+                throw new Exception(String.Format("negatives not allowed: {0}", String.Join(",", negatives)));
+            }
+            return result;
+        }
+    }
+}
---
+++ SogetiAssessment.Tests/StringCalculatorTests.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SogetiAssessment.Console;
+
+namespace SogetiAssessment.Tests
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        StringCalculator calculator;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            calculator = new StringCalculator();
+        }
+
+        [TestMethod]
+        public void EmptyStringReturns0()
+        {
+            string input = "";
+            int expected = 0;
+            var result = calculator.Add(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void OneNumberReturnsSame()
+        {
+            string input = "1";
+            int expected = 1;
+            var result = calculator.Add(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void OnePlusTwoEqualsThree()
+        {
+            string input = "1,2";
+            int expected = 3;
+            var result = calculator.Add(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TenNumbers()
+        {
+            string input = "1,2,3,4,5,6,7,8,9,10";
+            int expected = 55;
+            var result = calculator.Add(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ArbitraryNumbers()
+        {
+            var rnd = new Random();
+            int numValues = rnd.Next(100);
+            string[] input = new string[numValues];
+            int expected = 0;
+            for (var i = 0; i < numValues; i++)
+            {
+                input[i] = i.ToString();
+                expected += i;
+            }
+            var result = calculator.Add(String.Join(",", input));
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Newlines()
+        {
+            string input = "1\n2,3";
+            int expected = 6;
+            var result = calculator.Add(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void DefineDelimiter()
+        {
+            string input = "//;\n1;2";
+            int expected = 3;
+            var result = calculator.Add(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void NegativesNotAllowedOneNegative()
+        {
+            string input = "-1";
+            string expectedException = "negatives not allowed: -1";
+            try
+            {
+                var result = calculator.Add(input);
+                Assert.Fail();
+            }
+            catch(Exception ex)
+            {
+                Assert.AreEqual(expectedException, ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void NegativesNotAllowedMultipleNegatives()
+        {
+            string input = "-1,2,-3,4";
+            string expectedException = "negatives not allowed: -1,-3";
+            try
+            {
+                var result = calculator.Add(input);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(expectedException, ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void IgnoreThousands()
+        {
+            string input = "2,1001";
+            int expected = 2;
+            var result = calculator.Add(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AnyLengthDelimiter()
+        {
+            string input = "//[***]\n1***2***3";
+            int expected = 6;
+            var result = calculator.Add(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void MultipleArbitraryDelimiters()
+        {
+            string input = "//[*][%]\n1*2%3";
+            int expected = 6;
+            var result = calculator.Add(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
